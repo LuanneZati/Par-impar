414e26bd350bed53a7b55968242758f880816d17
LuanneZati <luanezattilima@hotmail.com>
2023-01-20 09:33:03 -0300

    first commit

---
+++ ParImpar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    public class ParImpar
+    {
+        private double number;
+        public double Number{  set { number = value; } }
+
+        public void DefineParidade()
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is par.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is impar.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Algoritmo que solicita a entrada de um número e calcule se o número é ímpar ou par
+
+using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParImpar parImpar = new ParImpar();
+            Console.Write("Enter a number: ");
+            parImpar.Number = Convert.ToDouble(Console.ReadLine());
+            parImpar.DefineParidade();
+        }
+    }
+    
+}
